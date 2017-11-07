e85650622d3039be9f53cb2a3d2e863d85ab138e
Alej. N. Pérez <alxegod@gmail.com>
2017-11-07 12:54:23 +0100

    Uploaded new project base

---
+++ app/src/main/java/com/example/practicanpi/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.practicanpi;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import ai.api.android.AIConfiguration;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.ui.AIDialog;
+
+public class MainActivity extends AppCompatActivity implements AIDialog.AIDialogListener {
+
+    private AIDialog aiDialog;
+
+    private boolean checkAndRequestRecordAudioPermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // If  an explanation is required, show it
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO))
+                Toast.makeText(getApplicationContext(), "Need microphone to record", Toast.LENGTH_SHORT).show();
+
+            // Request the permission.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ Manifest.permission.RECORD_AUDIO },0);
+        }
+
+        return ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final AIConfiguration config = new AIConfiguration("1973329acf964ea48e5d08e04d2c08a0",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiDialog = new AIDialog(this, config);
+        aiDialog.setResultsListener(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkAndRequestRecordAudioPermission()) aiDialog.showAndListen();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResult(AIResponse result) {
+        Toast.makeText(null, "Result recieved", Toast.LENGTH_LONG)
+            .show();
+    }
+
+    @Override
+    public void onError(AIError error) {
+        Log.e(getClass().getSimpleName(), "Error on listen");
+    }
+
+    @Override
+    public void onCancelled() {
+        Log.i(getClass().getSimpleName(), "Canceled recording");
+    }
+}
